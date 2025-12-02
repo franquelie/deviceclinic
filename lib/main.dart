a0970bb81082687bc321a641d6e7dd151d67a4e8
Franquelie Masanque <franquelie@gmail.com>
2025-12-02 09:03:03 +0800

    Setup Firebase for iOS app

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'brands/laptop_brands.dart';
 import 'brands/phone_brands.dart';
 import 'brands/tablet_brands.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
